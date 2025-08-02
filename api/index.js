a35c5775958bc14182e9a2545351fc6c2f233ebf
OjiChanERO <151846161+OjiChanERO@users.noreply.github.com>
2025-08-03 02:01:03 +0930

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,7 +2,6 @@ const { API } = require('nhentai-api');
 const api = new API();
 
 // This is the Vercel Serverless Function handler.
-// It will be available at your-vercel-url.com/api
 module.exports = async (req, res) => {
     // Set CORS headers to allow your index.html to talk to this function.
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -16,7 +15,6 @@ module.exports = async (req, res) => {
     }
 
     // Extract the query parameters from the URL.
-    // Example: /api?q=search&term=fate&page=1
     const { q, term, id, page } = req.query;
 
     try {
@@ -25,23 +23,27 @@ module.exports = async (req, res) => {
             case 'search':
                 data = await api.search(term || '', parseInt(page) || 1);
                 break;
+            
+            // *** THIS IS THE FIX ***
+            // The 'popular' query now correctly searches for an empty string.
             case 'popular':
-                data = await api.getPopular();
+                data = await api.search('', parseInt(page) || 1);
                 break;
+
             case 'info':
                 if (!id) throw new Error('An ID must be provided to get info.');
                 const book = await api.getBook(id);
                 // The nhentai-api library doesn't include full page URLs, so we construct them.
                 const pages = book.pages.map((p, i) => {
-                    // Determine the file extension
                     const ext = p.t === 'j' ? 'jpg' : 'png';
                     return `https://i.nhentai.net/galleries/${book.mediaId}/${i + 1}.${ext}`;
                 });
                 data = { ...book, page_images: pages };
                 break;
+
             default:
                 // Default to popular if no query or an unknown query is provided
-                data = await api.getPopular();
+                data = await api.search('', 1);
         }
         res.status(200).json(data);
     } catch (error) {
